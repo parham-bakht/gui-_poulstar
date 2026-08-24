428a606b419b1015ac75dba302420f83cf7f6895
parham-bakht <parhambakhtiari1380@gmail.com>
2026-08-24 18:32:11 +0330

    new

---
+++ arzotala.py
@@ -0,0 +1,5 @@
+import requests
+from customtkinter import *
+currency_api = "https://api.one-api.ir/currency/v1/navasan/currency"
+gold_api = "https://api.one-api.ir/currency/v1/navasan/gold"
+token = "993228:688a82b1bd11a"
